67d0b8e6a125ad7e7321ec167eccee14982d9ec0
hudsonbay <manuelmenendezalfonso@gmail.com>
2021-03-30 14:09:01 -0400

    Preloaded data

--- lib/metaito_api/accounts.ex
+++ lib/metaito_api/accounts.ex
@@ -57,7 +57,7 @@ defmodule MetaitoApi.Accounts do
       ** (Ecto.NoResultsError)
 
   """
-  def get_user!(id), do: Repo.get!(User, id)
+  def get_user!(id), do: Repo.get!(User, id) |> Repo.preload([:notes, :links])
 
   ## User registration
 
