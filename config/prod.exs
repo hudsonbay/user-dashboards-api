839d422bba8219fedc4fea3c9c9ef79f0c7afc9f
hudsonbay <manuelmenendezalfonso@gmail.com>
2021-04-20 02:00:06 -0400

    Added PORT

--- config/prod.exs
+++ config/prod.exs
@@ -11,6 +11,7 @@ use Mix.Config
 # before starting your production server.
 config :metaito_api, MetaitoApiWeb.Endpoint,
   load_from_system_env: true,
+  http: [:inet6, port: System.get_env("PORT") || 4000],
   url: [scheme: "https", host: "user-dashboard-elixir-phoenix.herokuapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json",
