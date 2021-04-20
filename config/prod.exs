8e60ad761a902ff719ee34ff9b60967ec108395a
hudsonbay <manuelmenendezalfonso@gmail.com>
2021-04-20 01:49:25 -0400

    New deploy configuration

--- config/prod.exs
+++ config/prod.exs
@@ -11,12 +11,10 @@ use Mix.Config
 # before starting your production server.
 config :metaito_api, MetaitoApiWeb.Endpoint,
   load_from_system_env: true,
-  url: [scheme: "https", host: "notes-elixir-phoenix-api.herokuapp.com", port: 443],
+  url: [scheme: "https", host: "user-dashboard-elixir-phoenix.herokuapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
-
-
 
 # Do not print debug messages in production
 config :logger, level: :info
